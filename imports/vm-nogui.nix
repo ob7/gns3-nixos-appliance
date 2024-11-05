e1e6330df63a560038798a207a551cb0b68792ba
ob7 <dev@ob7.us>
2024-11-04 23:38:18 -0900

    automatically resize console based on size of tty at login, can also be triggered manually via resize command

---
+++ imports/vm-nogui.nix
@@ -0,0 +1,17 @@
+{pkgs, ...}: let
+  resize = pkgs.writeScriptBin "resize" ''
+    if [ -e /dev/tty ]; then
+      old=$(stty -g)
+      stty raw -echo min 0 time 5
+      printf '\033[18t' > /dev/tty
+      IFS=';t' read -r _ rows cols _ < /dev/tty
+      stty "$old"
+      stty cols "$cols" rows "$rows"
+    fi
+  ''; # https://unix.stackexchange.com/questions/16578/resizable-serial-console-window
+in {
+  environment.systemPackages = [resize];
+  programs.bash.loginShellInit = ''
+    ${resize}/bin/resize;
+  '';
+}
