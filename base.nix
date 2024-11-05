e0e798e4161c1c96f15584d4c7dd0f0fec47b47a
ob7 <dev@ob7.us>
2024-11-04 18:10:17 -0900

    base.nix, contains common configuration for all nixos-appliances

---
+++ base.nix
@@ -0,0 +1,38 @@
+{ config
+, pkgs
+, ... }:
+{
+  networking.usePredictableInterfaceNames = false;
+  environment.systemPackages = with pkgs; [
+  ];
+  environment.shellAliases = {
+    "x" = "exit";
+    "c" = "clear";
+    ".." = "cd ../";
+    "..." = "cd ../../";
+    "grep" = "grep --color -i";
+    "ack" = "ack -Q";
+    "ncat" = "nc";
+  };
+
+  services.getty.autologinUser = "root";
+  security.sudo.wheelNeedsPassword = false;
+  #networking.hostName = "server"; #default and should get overridden by systemd
+  users.extraUsers.cisco = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    initialPassword = "cisco";
+  };
+
+  #systemd service to set the hostname dynamically
+  systemd.services.set-dynamic-hostname = {
+    description = "Set hostname dynamically from QEMU fw_cfg";
+    after = [ "sysinit.target" ];
+    before = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.bash}/bin/bash -c 'if [ -f /sys/firmware/qemu_fw_cfg/by_name/opt/vm_hostname/raw ]; then hostname=$(cat /sys/firmware/qemu_fw_cfg/by_name/opt/vm_hostname/raw); echo \"$hostname\" > /proc/sys/kernel/hostname; fi'";
+    };
+  };
+}
