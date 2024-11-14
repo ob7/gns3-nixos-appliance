ce0784e9f008e59d514b60bd63ddad6b5679ab4d
ob7 <dev@ob7.us>
2024-11-14 09:13:20 -0900

    add web server

--- server.nix
+++ server.nix
@@ -23,10 +23,19 @@
     ./base.nix
   ];
 
-	networking = {
-		domain = "lab.local";
-		search = [ "lab.local" ];
-  };
-
   networking.firewall.enable = false;
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."web.this.lab" = {
+      listen = [ { addr = "0.0.0.0"; port = 80; } ];
+      locations."/" = {
+        extraConfig = ''
+          default_type text/html;
+          return 200 "<html><body><h1>Hello, NixOS!</h1><p>This page is served directly by Nginx without files.</p></body></html>";
+        '';
+      };
+    };
+  };
+ 
 }
